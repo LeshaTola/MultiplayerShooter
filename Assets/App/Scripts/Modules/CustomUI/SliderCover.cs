5f32ce44b51aa3fbf608264c57118254a329b098
LeshaTola <tola.lesha@mail.ru>
2025-01-15 20:20:08 +0300

    Remake Settings

---
+++ Assets/App/Scripts/Modules/CustomUI/SliderCover.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace App.Scripts.Modules.CustomToggles
+{
+    public class SliderCover:MonoBehaviour
+    {
+        [field: SerializeField] public Slider Slider { get; private set; }
+        [SerializeField] private TextMeshProUGUI _valueText;
+
+        private void Awake()
+        {
+            Slider.onValueChanged.AddListener(SetHandle);
+        }
+
+        private void Start()
+        {
+            SetHandle(Slider.value);
+        }
+
+        private void SetHandle(float value)
+        {
+            _valueText.text = ((int) (value * 100)).ToString();
+        }
+    }
+}
