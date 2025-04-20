bf6d95cf967bfc55b883689e88235d840a443913
LeshaTola <tola.lesha@mail.ru>
2025-04-20 17:42:50 +0300

    Add telegramm link

---
+++ Assets/App/Scripts/Scenes/MainMenu/Features/Screens/MainScreen/TelegrammButton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace App.Scripts.Scenes.MainMenu.Features.Screens.MainScreen
+{
+    public class TelegrammButton: MonoBehaviour
+    {
+        [SerializeField] private Button _button;
+
+        private void Awake()
+        {
+            _button.onClick.AddListener(OpenTelegramm);
+        }
+
+        private void OnDestroy()
+        {
+            _button.onClick.RemoveAllListeners();
+        }
+
+        private void OpenTelegramm()
+        {
+            Application.OpenURL("https://t.me/Robot_Wars");
+        }
+    }
+}
